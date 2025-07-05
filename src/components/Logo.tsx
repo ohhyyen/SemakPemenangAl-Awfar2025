c95b2be78a4febddd867c417a6a2f14c1614142f
[dyad] <git@dyad.sh>
2025-07-05 21:28:03 +0800

    [dyad] Menukar warna logo kepada putih - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Logo = () => {
   return (
     <Link to="/">
-      <img src="/BIMB-Logo-Main.png" alt="BIMB Logo" className="h-8 w-auto" />
+      <img src="/BIMB-Logo-Main.png" alt="BIMB Logo" className="h-8 w-auto filter brightness-0 invert" />
     </Link>
   );
 };
