aac635a401a5f1af08c0c2ebdc4660c83e4e0b85
[dyad] <git@dyad.sh>
2025-07-05 21:20:33 +0800

    Reverted all changes back to version 790c5d0e79acba3533cf54b8c3ae356843dc0135

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 export const Logo = () => {
   return (
     <Link to="/">
-      <img src="/BIMB-Logo-Main.png" alt="BIMB Logo" className="h-8 w-auto" />
+      <div className="bg-white text-black font-bold p-2">
+        LOGO DI SINI
+      </div>
     </Link>
   );
 };
